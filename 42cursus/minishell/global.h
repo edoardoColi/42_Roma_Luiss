9dc93571e9d605eb5a58d40d1a99f19142b12a09
edoardoColi <edoardo.coli98@gmail.com>
2023-03-23 00:46:16 +0100

    fix

--- 42cursus/minishell/global.h
+++ 42cursus/minishell/global.h
@@ -6,7 +6,7 @@
 /*   By: eddy <eddy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 12:15:39 by ecoli             #+#    #+#             */
-/*   Updated: 2023/03/22 20:52:01 by eddy             ###   ########.fr       */
+/*   Updated: 2023/03/23 00:43:56 by eddy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,9 +45,7 @@ typedef struct s_command
 {
 	char	**args;
 	char	**rin_and_heredoc;	// < e <<
-	char	**heredoc;	// <<
 	char	**rout_and_append;	// > e >>
-	char	**append;	// >>
 	int		*red_type;
 	int		fd[2];
 }	t_command;
